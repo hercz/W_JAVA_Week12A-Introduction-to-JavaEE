16380b8204b86031a41696172724d1bb7dd04a26
Herczku Misi <kmisku@gmail.com>
2016-03-08 13:43:24 +0100

    A new START.

--- src/servlets/Login.java
+++ src/servlets/Login.java
@@ -1,10 +1,9 @@
-package servlet;
+package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,34 +12,34 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class Login
  */
-@WebServlet("/login")
+// @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String userName, password;
 		String defUserName = "kacsa";
 		String defPassword = "1234";
 
+		response.setContentType("text/html");
+
 		PrintWriter print = response.getWriter();
 
 		userName = request.getParameter("userName");
 		password = request.getParameter("password");
-		HttpSession session = request.getSession();
 
 		if (password.equals(defPassword) && userName.equals(defUserName)) {
+			print.print("Welcome " + userName + " friend!");
+			HttpSession session = request.getSession();
 			session.setAttribute("user", userName);
-			response.sendRedirect("Profile.jsp");
+			request.getRequestDispatcher("profile.html").include(request, response);
+
 		} else {
 			print.print("Bad username or password!");
-			request.getRequestDispatcher("Login.jsp").include(request, response);
+			request.getRequestDispatcher("login.html").include(request, response);
 		}
+		print.close();
 
 	}
-
 }
--- src/servlets/Profile.java
+++ src/servlets/Profile.java
@@ -1,39 +1,35 @@
-package servlet;
+package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LogOut
- */
-@WebServlet("/LogOut")
-public class LogOut extends HttpServlet {
+//@WebServlet("/Profile")
+public class Profile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		request.getRequestDispatcher("profile.html").include(request, response);
 
-		request.getRequestDispatcher("Login.jsp").include(request, response);
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			String name = (String) session.getAttribute("name");
 
-		HttpSession session = request.getSession();
-		session.invalidate();
-
-		out.print("You are successfully logged out!");
-
+			out.print("Hello, " + name + " Welcome to Profile");
+		} else {
+			out.print("Please login first");
+			request.getRequestDispatcher("login.html").include(request, response);
+		}
 		out.close();
+
 	}
 
 }
